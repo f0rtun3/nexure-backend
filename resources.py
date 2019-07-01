40fc24b2b963d2d112d49101ad110f790257408d
Paul Odera <oderapaul7@gmail.com>
2019-07-01 13:38:21 +0300

    user account confirmation handler

--- resources.py
+++ resources.py
@@ -11,7 +11,10 @@ from flask_restful import Resource, reqparse
 # get the utility file
 import helpers.helpers as helper
 import helpers.tokens as token_handler
+#jwt token authentication library
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
+# from app import app
 import uuid
 
 user_parser = reqparse.RequestParser()
@@ -75,18 +78,19 @@ class UserRegister(Resource):
             Roles.fetch_role_by_name(user_details['role'])
         )
         new_user_role.save()
-        """
-        Account confirmation email generation
+        confirmation_code = token_handler.user_account_confirmation_token(new_user_authentication.id)
+        # Account confirmation email generation
         #   we need to generate a code for user to use for confirmation code
-        #   confirmation_code = token_handler.user_account_confirmation_token(new_user_authentication.id)
-        #   email_template = helper.generate_confirmation_template(app.config['CONFIRMATION_ENDPOINT'],
-        #                                                          confirmation_code)
-        #   subject = "Please confirm your account"
-        #   helper.send_email(user_details['email'], subject, email_template)
+        """
+        email_template = helper.generate_confirmation_template(app.config['CONFIRMATION_ENDPOINT'],
+                                                                  confirmation_code)
+        subject = "Please confirm your account"
+        helper.send_email(user_details['email'], subject, email_template)
         """
         #   ToDo: Configure email server and uncomment the above section
+
         success_msg = "You have been registered. Kindly check your email to confirm account"
-        response = helper.make_rest_success_response(success_msg)
+        response = helper.make_rest_success_response(success_msg, {"confirmation_token": confirmation_code})
 
         return make_response(response, 200)
 
@@ -104,8 +108,50 @@ class UserRegister(Resource):
         """
 
 
-class UserAccountConfirmation:
-    pass
+class UserAccountConfirmation(Resource):
+    @jwt_required
+    def put(self):
+        """
+        authenticaiton token is sent here for confirmation
+        token must be valid for account to be activated
+        """
+        # the user id is needed to needed to know the user whose account we are activating
+        user_id =  get_jwt_identity()
+        user_row = User.get_user_by_id(user_id)
+        if user_row:
+            if user_row.is_active == True:
+                response = helper.make_rest_success_response("Your account is already active, please login")
+                return make_response(response, 200)    
+            data = {}
+            data['is_active'] = True
+            user_row.update(data)
+            response = helper.make_rest_success_response("Your account has been activated, you can now log in")
+            return make_response(response, 200)
+
+        # the user has not been found in the database
+        response = helper.make_rest_fail_response("User does not exist")
+        return make_response(response, 404)
+
+    def get(self, user_id):
+        """
+        If the jwt token has expired
+        a user can request for another token here simple by passing in the user_id
+        """
+        """
+        user_row = User.get_user_by_id(user_id)
+        if user_row:
+            confirmation_code = token_handler.user_account_confirmation_token(user_id)
+            email_template = helper.generate_confirmation_template(app.config['CONFIRMATION_ENDPOINT'],
+                                                                  confirmation_code)
+            subject = "Please confirm your account"
+            helper.send_email(user_row.email, subject, email_template)
+            response = helper.make_rest_success_response("Please check your email to confirm your account")
+            return make_response(response, 200)
+
+        response = helper.make_rest_fail_response("User was not found, please try again or register a new account")
+        return make_response(response, 404)
+        """
+        pass
 
 
 class UserLogin(Resource):
