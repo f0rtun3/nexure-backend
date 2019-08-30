778bc347283958fd0db62dfbfd764f599a6be49d
Paul Odera <oderapaul7@gmail.com>
2019-08-30 11:50:19 +0300

    company details relationship correction

--- models/CompanyDetails.py
+++ models/CompanyDetails.py
@@ -11,7 +11,7 @@ class CompanyDetails(db.Model):
     physical_address = db.Column(db.String(300))
     website = db.Column(db.String(150))
     avatar = db.Column(db.String(50), nullable=True)
-    master_policies = db.relationship("MasterPolicy", backref="master_policies")
+    # master_policies = db.relationship("MasterPolicy", backref="master_policies")
 
     def __init__(self, company_name, company_email, physical_address, website, avatar=None):
         self.company_name = company_name
