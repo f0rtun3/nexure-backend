c10a595f5de6ff91874c41955719d8d05801575f
Paul Odera <oderapaul7@gmail.com>
2019-09-19 10:07:05 +0300

    locations get request reformat

--- resources/Location.py
+++ resources/Location.py
@@ -8,11 +8,13 @@ from helpers import helpers as helper
 
 class Location(Resource):
     def get(self):
-
         counties = County.get_all_counties()
-        if counties:
-            response = helper.make_rest_success_response("Success", counties)
-            return make_response(response, 200)
-
-        return make_response(helper.make_rest_fail_response(
-                            "No data was found"), 404)
+        constituencies = Constituency.get_all_constituencies()
+        wards = Ward.get_all_wards()
+        response = {
+            "counties": counties,
+            "constituencies": constituencies,
+            "wards": wards
+        }
+        response = helper.make_rest_success_response("Success", response)
+        return make_response(response, 200)
